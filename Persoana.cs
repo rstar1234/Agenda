5e21f4d17f0a6ee32e5fbd6a76d6846e1d4bc07f
raluca_stamate <raluca.stamate@student.usv.ro>
2023-03-14 16:41:23 +0200

    Add project files.

---
+++ Persoana.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Persoana
+    {
+        private string nume, email, grup;
+        private int numarDeTelefon;
+        private DateTime ziDeNastere;
+        public Persoana(string _nume, string _email, string _grup, int _numarDeTelefon, DateTime _ziDeNastere)
+        {
+            nume = _nume;
+            email = _email;
+            grup = _grup;
+            numarDeTelefon = _numarDeTelefon;
+            ziDeNastere = _ziDeNastere;
+        }
+    }
+}
